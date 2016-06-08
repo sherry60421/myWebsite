1cc8dc8a9f9d046a17687721946752bb2ad72048
sherry60421 <sherry60421@gmail.com>
2016-06-09 03:16:12 +0800

    add touchevent

--- js/index.js
+++ js/index.js
@@ -1,3 +1,41 @@
+// iphone touch行為
+function touchHandler(event)
+{
+    var touches = event.changedTouches,
+        first = touches[0],
+        type = "";
+
+    switch(event.type)
+    {
+       case "touchstart": type = "mousedown"; break;
+       case "touchmove":  type = "mousemove"; break;
+       case "touchend":   type = "mouseup"; break;
+       default: return;
+    }
+
+    //initMouseEvent(type, canBubble, cancelable, view, clickCount,
+    //           screenX, screenY, clientX, clientY, ctrlKey,
+    //           altKey, shiftKey, metaKey, button, relatedTarget);
+
+    var simulatedEvent = document.createEvent("MouseEvent");
+    simulatedEvent.initMouseEvent(type, true, true, window, 1,
+                          first.screenX, first.screenY,
+                          first.clientX, first.clientY, false,
+                          false, false, false, 0/*left*/, null);
+
+    first.target.dispatchEvent(simulatedEvent);
+    event.preventDefault();
+}
+
+function init()
+{
+    document.addEventListener("touchstart", touchHandler, true);
+    document.addEventListener("touchmove", touchHandler, true);
+    document.addEventListener("touchend", touchHandler, true);
+    document.addEventListener("touchcancel", touchHandler, true);
+}
+init();
+// --------------------------------
 var tagOrder = ['tag-primary', 'tag-success', 'tag-warning', 'tag-danger'];
 var isOpenMenu = false;// for mobile
 // 抓現有的category建立folder tag(待加文章!!!)
