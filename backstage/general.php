2beaf816d47dfa7fc54490a12e3e683d2bc406fc
sherry60421 <sherry60421@gmail.com>
2016-06-09 10:29:10 +0800

    調整DBConfig+去掉FB

--- backstage/general.php
+++ backstage/general.php
@@ -2,11 +2,18 @@
 
 header("Content-Type:text/html; charset=utf-8");
 
-$host = 'localhost';
-$dbUsername = 'adminCMBRcMd'; // 2141104_myweb / adminCMBRcMd
-$dbPassword = 'yk819N-UCj3x'; // MxxBDNqJPv0626 / yk819N-UCj3x
-$dbName = 'myweb'; // 2141104_myweb / myweb
-
+if($_SERVER["HTTP_HOST"] == "localhost"){
+  $host = 'localhost';
+  $dbUsername = '2141104_myweb'; // 2141104_myweb / adminCMBRcMd
+  $dbPassword = 'MxxBDNqJPv0626'; // MxxBDNqJPv0626 / yk819N-UCj3x
+  $dbName = '2141104_myweb'; // 2141104_myweb / myweb
+}
+else{
+  $host = 'localhost';
+  $dbUsername = 'adminCMBRcMd'; // 2141104_myweb / adminCMBRcMd
+  $dbPassword = 'yk819N-UCj3x'; // MxxBDNqJPv0626 / yk819N-UCj3x
+  $dbName = 'myweb'; // 2141104_myweb / myweb
+}
 $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
 $conn->set_charset("utf8");
 echo $conn->error;
